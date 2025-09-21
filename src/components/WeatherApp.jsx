554dc14e3e805f3831b493e2c45e689267f1b46e
PedroDonini <pedro.donini96@gmail.com>
2025-09-21 19:06:14 -0300

    Componente de Busca

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,7 +1,10 @@
+import WeatherSearch from "./WeatherSearch";
+
 export default function WeatherApp(){
     return (
         <div>
             <h1>Weather App</h1>
+            <WeatherSearch />
         </div>
     );
 }
---
+++ src/components/WeatherSearch.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react" ;
+
+export default function WeatherSearch(){
+    const [city, setCity] = useState("") ;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(city.trim()){
+            // TODO: Lógica para consumir os dados da API -> Contexto para retornar informações de clima da cidade buscada
+        }
+    }  
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Digite o nome da cidade"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+            />
+            <button type="submit">Buscar</button>
+        </form>
+    )
+}
