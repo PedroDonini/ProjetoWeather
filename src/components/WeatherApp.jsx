3e2892a6a5ef3fbe0562c9a2238b27f2ec506632
PedroDonini <pedro.donini96@gmail.com>
2025-09-24 19:56:59 -0300

    Ajustando texto

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -2,10 +2,10 @@ import WeatherSearch from "./WeatherSearch";
 import WeatherDisplay from "./WeatherDisplay";
 import WeatherChart from "./WeatherChart";
 
-export default function WeatherApp(){
+export default function WeatherApp(){   
     return (
         <div>
-            <h1>Weather App</h1>
+            <h1 style={{textAlign: 'center', marginBottom: '20px'}}>WEATHER APP</h1>
             <WeatherSearch />
             <WeatherDisplay />
             <WeatherChart />
